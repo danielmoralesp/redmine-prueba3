b73b1bb816425b056fba1e734c5945da07e54afd
danielmoralesp <daniel_moralesp@hotmail.com>
2017-09-29 15:38:12 -0500

    Controller hook

--- plugins/redmine_knowledgebase/lib/hooks.rb
+++ plugins/redmine_knowledgebase/lib/hooks.rb
@@ -12,3 +12,28 @@ module RedmineKnowledgebase
     end
   end
 end
+
+module Knowledgebase
+     module Hooks
+       class ControllerIssuesEditBeforeSaveHook < Redmine::Hook::ViewListener
+         def controller_issues_edit_before_save(context={})
+           if context[:params] && context[:params][:issue]
+             if User.current.allowed_to?(:assign_article_to_issue,
+               context[:issue].project)
+               if context[:params][:issue][:article_id].present?
+                 article = KbArticle.find_by_id(context[:params]
+                   [:issue][:article_id])
+                   if article.category.project ==
+                    context[:issue].project
+                    context[:issue].article = article
+                  end
+                  else
+              context[:issue].article = nil
+            end
+          end
+        end
+        return ''
+      end
+    end
+  end
+end
