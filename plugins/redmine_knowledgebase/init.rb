e436beef8a036899d934f0238caac1ab7f54cb62
danielmoralesp <daniel_moralesp@hotmail.com>
2017-09-29 15:31:58 -0500

    Views hook

--- plugins/redmine_knowledgebase/init.rb
+++ plugins/redmine_knowledgebase/init.rb
@@ -1,13 +1,4 @@
 Redmine::Plugin.register :redmine_knowledgebase do
-  name 'Redmine Knowledgebase plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
-end
-
-Redmine::Plugin.register :redmine_knowledgebase do
   name        'Knowledgebase'
   author      'Alex Bevilacqua'
   author_url  'http://www.alexbevi.com'
